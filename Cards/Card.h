a83390059f4951cfe9d4c471872e279f07896d2e
pinirus <zuvado11@yandex.ua>
2022-01-14 00:36:56 +0200

    Add Lib

---
+++ Cards/Card.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Card
+{
+public:
+
+	Card()
+	{
+	}
+
+	sf::RectangleShape Shape;
+	
+
+protected:
+
+};
---
+++ GameClient/Entrance.cpp
@@ -0,0 +1,5 @@
+﻿#include "Card.h"
+
+
+int main() {
+}
